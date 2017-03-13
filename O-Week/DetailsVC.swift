1447af60233114a28986ce69e2039b3480e47f84
Vicente Caycedo <vicentecaycedo@dhcp-ccc-1408.eduroam.cornell.edu>
2017-03-13 12:15:46 -0400

    Further edited Details Page

---
+++ O-Week/DetailsVC.swift
@@ -0,0 +1,22 @@
+//
+//  DetailsVC.swift
+//  O-Week
+//
+//  Created by Vicente Caycedo on 3/13/17.
+//  Copyright © 2017 Cornell SA Tech. All rights reserved.
+//
+
+import UIKit
+
+class DetailsVC: UIViewController {
+    
+    //TODO: fix constraints
+    
+    @IBOutlet weak var image: UIImageView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
