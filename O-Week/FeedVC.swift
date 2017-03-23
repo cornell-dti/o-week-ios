4ab7d065f4abfd871a290fbdc71ff6359664b3ed
Vicente Caycedo <vicentecaycedo@dhcp-clara-322.eduroam.cornell.edu>
2017-03-22 21:44:38 -0400

    cleaned up code

--- O-Week/FeedVC.swift
+++ O-Week/FeedVC.swift
@@ -11,23 +11,15 @@ import UIKit
 class FeedVC:UIViewController, UITableViewDelegate, UITableViewDataSource, UIGestureRecognizerDelegate
 {
     
-    @IBOutlet weak var feedTableView: UITableView!
-    @IBOutlet weak var view0: UIView!
-    @IBOutlet weak var view1: UIView!
-    @IBOutlet weak var view2: UIView!
-    @IBOutlet weak var view3: UIView!
-    @IBOutlet weak var view4: UIView!
-    @IBOutlet weak var label0: UILabel!
-    @IBOutlet weak var label1: UILabel!
-    @IBOutlet weak var label2: UILabel!
-    @IBOutlet weak var label3: UILabel!
-    @IBOutlet weak var label4: UILabel!
+    //MARK:- Properties
     
-    var views: [UIView] = []
-    var labels: [UILabel] = []
+    @IBOutlet weak var feedTableView: UITableView!
+    @IBOutlet var views: [UIView]!
+    @IBOutlet var labels: [UILabel]!
+
     var selected: Int = 0 //index of date selected (0-4)
     
-    //Temporary Data
+    // FIXME: Temporary Data
     let data = [["7:45 AM", "8:45 AM", "Alumni Families and Legacy Reception", "Tent on Rawlings Green"],
                 ["8:45 AM", "10:00 AM", "New Student Convocation", "Shoellkopf Stadium"],
                 ["10:00 AM", "11:30 AM", "Tours of Libraries and Manuscript", "Upper Lobby, Uris Library"],
@@ -35,73 +27,21 @@ class FeedVC:UIViewController, UITableViewDelegate, UITableViewDataSource, UIGes
                 ["10:30 AM", "11:30 AM", "AAP—Dean’s Convocation", "Abby and Howard Milstein Hall"],
                 ["10:30 AM", "11:30 AM", "CALS—Dean’s Convocation", "Call Alumni Auditorium, Kennedy Hall"]]
     
+    // MARK:- Setup
+    
     override func viewDidLoad()
     {
         super.viewDidLoad()
         setUpExtendedNavBar()
         setUpHeightofFeedCell()
-        setUpNavBarViewsandLabels()
         setUpGestureRecognizers()
     }
     
-    func setUpNavBarViewsandLabels(){
-        views = [view0, view1, view2, view3, view4]
-        labels = [label0, label1, label2, label3, label4]
-        for view in views{
-            view.layer.cornerRadius = 18 //half of width for a perfect circle
-        }
-        setUpNavBarViewsandLabelsWithSelected(selected: 0)
-    }
-    
     func setUpGestureRecognizers(){
-        //clean up code later
-        let gr0 = UITapGestureRecognizer(target: self, action: #selector(self.handleTap0(_:)))
-        let gr1 = UITapGestureRecognizer(target: self, action: #selector(self.handleTap1(_:)))
-        let gr2 = UITapGestureRecognizer(target: self, action: #selector(self.handleTap2(_:)))
-        let gr3 = UITapGestureRecognizer(target: self, action: #selector(self.handleTap3(_:)))
-        let gr4 = UITapGestureRecognizer(target: self, action: #selector(self.handleTap4(_:)))
-        view0.addGestureRecognizer(gr0)
-        view1.addGestureRecognizer(gr1)
-        view2.addGestureRecognizer(gr2)
-        view3.addGestureRecognizer(gr3)
-        view4.addGestureRecognizer(gr4)
-        view0.isUserInteractionEnabled = true
-        view1.isUserInteractionEnabled = true
-        view2.isUserInteractionEnabled = true
-        view3.isUserInteractionEnabled = true
-        view4.isUserInteractionEnabled = true
-    }
-    
-    func handleTap0(_ sender: UITapGestureRecognizer) {
-        setUpNavBarViewsandLabelsWithSelected(selected: 0)
-        //TODO: implement button functionality for selected date
-    }
-    func handleTap1(_ sender: UITapGestureRecognizer) {
-        setUpNavBarViewsandLabelsWithSelected(selected: 1)
-        //TODO: implement button functionality for selected date
-    }
-    func handleTap2(_ sender: UITapGestureRecognizer) {
-        setUpNavBarViewsandLabelsWithSelected(selected: 2)
-        //TODO: implement button functionality for selected date
-    }
-    func handleTap3(_ sender: UITapGestureRecognizer) {
-        setUpNavBarViewsandLabelsWithSelected(selected: 3)
-        //TODO: implement button functionality for selected date
-    }
-    func handleTap4(_ sender: UITapGestureRecognizer) {
-        setUpNavBarViewsandLabelsWithSelected(selected: 4)
-        //TODO: implement button functionality for selected date
-    }
-    
-    func setUpNavBarViewsandLabelsWithSelected(selected: Int){
-        for index in 0...views.count - 1 {
-            if(index == selected){
-                views[index].backgroundColor = UIColor.white
-                labels[index].textColor = UIColor.black
-            }else{
-                views[index].backgroundColor = Color.RED
-                labels[index].textColor = UIColor.white
-            }
+        for view in views {
+            let gr = UITapGestureRecognizer(target: self, action: #selector(self.handleTap(_:)))
+            view.addGestureRecognizer(gr)
+            view.isUserInteractionEnabled = true
         }
     }
     
@@ -113,7 +53,40 @@ class FeedVC:UIViewController, UITableViewDelegate, UITableViewDataSource, UIGes
     func setUpExtendedNavBar(){
         navigationController?.navigationBar.shadowImage = UIImage(named: "transparent_pixel")
         navigationController?.navigationBar.setBackgroundImage(UIImage(named: "pixel"), for: .default)
+        for view in views{
+            view.layer.cornerRadius = 18 //half of width for a perfect circle
+        }
+        changeSelectedDate(to: 0)
     }
+    
+    // MARK:- Date Actions
+    
+    func handleTap(_ sender: UITapGestureRecognizer) {
+        //TODO: implement filtering functionality for selected date
+        switch sender.view {
+            case views[0]?: changeSelectedDate(to: 0)
+            case views[1]?: changeSelectedDate(to: 1)
+            case views[2]?: changeSelectedDate(to: 2)
+            case views[3]?: changeSelectedDate(to: 3)
+            case views[4]?: changeSelectedDate(to: 4)
+            default: break
+        }
+        
+    }
+    
+    func changeSelectedDate(to selected: Int){
+        for index in 0...views.count - 1 {
+            if(index == selected){
+                views[index].backgroundColor = UIColor.white
+                labels[index].textColor = UIColor.black
+            }else{
+                views[index].backgroundColor = Color.RED
+                labels[index].textColor = UIColor.white
+            }
+        }
+    }
+    
+    // MARK:- Table View Methods
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
     {
--- O-Week/ScheduleVC.swift
+++ O-Week/ScheduleVC.swift
@@ -10,27 +10,20 @@ import UIKit
 
 class ScheduleVC: UIViewController {
     
-    @IBOutlet weak var view0: UIView!
-    @IBOutlet weak var view1: UIView!
-    @IBOutlet weak var view2: UIView!
-    @IBOutlet weak var view3: UIView!
-    @IBOutlet weak var view4: UIView!
-    @IBOutlet weak var label0: UILabel!
-    @IBOutlet weak var label1: UILabel!
-    @IBOutlet weak var label2: UILabel!
-    @IBOutlet weak var label3: UILabel!
-    @IBOutlet weak var label4: UILabel!
+    // MARK:- Properties
     
-    var views: [UIView] = []
-    var labels: [UILabel] = []
+    @IBOutlet var views: [UIView]!
+    @IBOutlet var labels: [UILabel]!
+    
     var selected: Int = 0 //index of date selected (0-4)
+    
+    // MARK:- Setup
     
     override func viewDidLoad()
     {
         super.viewDidLoad()
         setUpNavBar()
         setUpExtendedNavBar()
-        setUpNavBarViewsandLabels()
         setUpGestureRecognizers()
     }
     
@@ -41,58 +34,36 @@ class ScheduleVC: UIViewController {
     func setUpExtendedNavBar(){
         navigationController?.navigationBar.shadowImage = UIImage(named: "transparent_pixel")
         navigationController?.navigationBar.setBackgroundImage(UIImage(named: "pixel"), for: .default)
-    }
-    
-    func setUpNavBarViewsandLabels(){
-        views = [view0, view1, view2, view3, view4]
-        labels = [label0, label1, label2, label3, label4]
         for view in views{
             view.layer.cornerRadius = 18 //half of width for a perfect circle
         }
-        setUpNavBarViewsandLabelsWithSelected(selected: 0)
+        changeSelectedDate(to: 0)
     }
     
     func setUpGestureRecognizers(){
-        //clean up code later
-        let gr0 = UITapGestureRecognizer(target: self, action: #selector(self.handleTap0(_:)))
-        let gr1 = UITapGestureRecognizer(target: self, action: #selector(self.handleTap1(_:)))
-        let gr2 = UITapGestureRecognizer(target: self, action: #selector(self.handleTap2(_:)))
-        let gr3 = UITapGestureRecognizer(target: self, action: #selector(self.handleTap3(_:)))
-        let gr4 = UITapGestureRecognizer(target: self, action: #selector(self.handleTap4(_:)))
-        view0.addGestureRecognizer(gr0)
-        view1.addGestureRecognizer(gr1)
-        view2.addGestureRecognizer(gr2)
-        view3.addGestureRecognizer(gr3)
-        view4.addGestureRecognizer(gr4)
-        view0.isUserInteractionEnabled = true
-        view1.isUserInteractionEnabled = true
-        view2.isUserInteractionEnabled = true
-        view3.isUserInteractionEnabled = true
-        view4.isUserInteractionEnabled = true
+        for view in views {
+            let gr = UITapGestureRecognizer(target: self, action: #selector(self.handleTap(_:)))
+            view.addGestureRecognizer(gr)
+            view.isUserInteractionEnabled = true
+        }
     }
     
-    func handleTap0(_ sender: UITapGestureRecognizer) {
-        setUpNavBarViewsandLabelsWithSelected(selected: 0)
-        //TODO: implement button functionality for selected date
-    }
-    func handleTap1(_ sender: UITapGestureRecognizer) {
-        setUpNavBarViewsandLabelsWithSelected(selected: 1)
-        //TODO: implement button functionality for selected date
-    }
-    func handleTap2(_ sender: UITapGestureRecognizer) {
-        setUpNavBarViewsandLabelsWithSelected(selected: 2)
-        //TODO: implement button functionality for selected date
-    }
-    func handleTap3(_ sender: UITapGestureRecognizer) {
-        setUpNavBarViewsandLabelsWithSelected(selected: 3)
-        //TODO: implement button functionality for selected date
-    }
-    func handleTap4(_ sender: UITapGestureRecognizer) {
-        setUpNavBarViewsandLabelsWithSelected(selected: 4)
-        //TODO: implement button functionality for selected date
+    // MARK:- Date Actions
+    
+    func handleTap(_ sender: UITapGestureRecognizer) {
+        //TODO: implement filtering functionality for selected date
+        switch sender.view {
+        case views[0]?: changeSelectedDate(to: 0)
+        case views[1]?: changeSelectedDate(to: 1)
+        case views[2]?: changeSelectedDate(to: 2)
+        case views[3]?: changeSelectedDate(to: 3)
+        case views[4]?: changeSelectedDate(to: 4)
+        default: break
+        }
+        
     }
     
-    func setUpNavBarViewsandLabelsWithSelected(selected: Int){
+    func changeSelectedDate(to selected: Int){
         for index in 0...views.count - 1 {
             if(index == selected){
                 views[index].backgroundColor = UIColor.white
