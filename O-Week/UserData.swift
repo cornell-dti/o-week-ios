11c1d8c0ae3920c2dcf8c534334fcac2a48da0e2
Vicente Caycedo <vac55@cornell.edu>
2017-04-30 13:04:25 -0400

    Set up files and code needed to use core data (files and code usually added when Use Core Data is checked in creation of project

--- O-Week/UserData.swift
+++ O-Week/UserData.swift
@@ -8,9 +8,112 @@
 //  Holds user's added events
 
 import Foundation
+import CoreData
 
 class UserData{
     
     static var selectedEvents:Set<Event> = Set()
     
+    //static var selectedEvents: Set<NSManagedObject> = Set()
+    
+    /*
+     Idea: on start up, download all event data and store w core data
+     Only let user change added var
+     
+     
+    cell.textLabel?.text = person.value(forKeyPath: "name") as? String
+    self.save(name: nameToSave)
+    
+     func save(name: String) {
+     
+     guard let appDelegate =
+     UIApplication.shared.delegate as? AppDelegate else {
+     return
+     }
+     
+     // Do in init or view did load
+     let managedContext =
+     appDelegate.persistentContainer.viewContext
+     let entity =
+     NSEntityDescription.entity(forEntityName: "Person",
+     in: managedContext)!
+     
+     
+     // for every person
+     let person = NSManagedObject(entity: entity,
+     insertInto: managedContext)
+     person.setValue(name, forKeyPath: "name")
+     do {
+        try managedContext.save()
+        people.append(person)
+     } catch let error as NSError {
+        print("Could not save. \(error), \(error.userInfo)")
+     }
+     }
+ 
+     //Retrieving data
+     guard let appDelegate =
+     UIApplication.shared.delegate as? AppDelegate else {
+     return
+     }
+     
+     let managedContext =
+     appDelegate.persistentContainer.viewContext
+     
+     //2
+     let fetchRequest =
+     NSFetchRequest<NSManagedObject>(entityName: "Person")
+     
+     //3
+     do {
+     people = try managedContext.fetch(fetchRequest)
+     } catch let error as NSError {
+     print("Could not fetch. \(error), \(error.userInfo)")
+     }
+     
+     //Deleting data
+     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+     
+     let noteEntity = "Note" //Entity Name
+     
+     let managedContext = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+     
+     let note = notes[indexPath.row]
+     
+     if editingStyle == .delete {
+     managedContext.delete(note)
+     
+     do {
+     try managedContext.save()
+     } catch let error as NSError {
+     print("Error While Deleting Note: \(error.userInfo)")
+     }
+     
+     }
+     
+     //Code to Fetch New Data From The DB and Reload Table.
+     let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: noteEntity)
+     
+     do {
+     notes = try managedContext.fetch(fetchRequest) as! [Note]
+     } catch let error as NSError {
+     print("Error While Fetching Data From DB: \(error.userInfo)")
+     }
+     noteTableView.reloadData()
+     }
+     
+     
+    */
+    
+    /*
+     Links
+     
+     Adding core data setup to existing project:
+     
+     Using core data:
+     https://www.raywenderlich.com/145809/getting-started-core-data-tutorial
+ 
+     
+     
+    */
 }
