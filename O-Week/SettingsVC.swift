898274944acb0f2831e3d44a827c699495282f71
Vicente Caycedo <vac55@cornell.edu>
2017-05-01 13:13:17 -0400

    Edited settings view theme

--- O-Week/SettingsVC.swift
+++ O-Week/SettingsVC.swift
@@ -10,8 +10,24 @@ import UIKit
 
 class SettingsVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
-    let data = [["Notifications", "Filters", "ETC"],
-                ["Notifications2", "Filters2", "ETC2"]]
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    let section_titles = ["Notifications", "Events"]
+    let data = [["Reminders set for" , "Notify me"],
+                ["Add all events to my schedule", "Add all required events to my schedule" ,"Remove all events from my schedule"]]
+    // Reminders set for - no events, all events, required events, custom
+    // Notify me - at time of event, 1 hour before, 2 hours before, 3 hours before, 5 hours before, morning of (7 am), 1 day before, 2 days before
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.backgroundView = nil
+        tableView.backgroundColor = UIColor.white
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return section_titles[section]
+    }
     
     func numberOfSections(in tableView: UITableView) -> Int {
         return data.count
@@ -25,6 +41,13 @@ class SettingsVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
         let cell = tableView.dequeueReusableCell(withIdentifier: "settingsCell") as! SettingsCell
         cell.label.text = data[indexPath.section][indexPath.row]
         return cell
+    }
+    
+    func tableView(_ tableView: UITableView, willDisplayHeaderView view: UIView, forSection section: Int)
+    {
+        let header = view as! UITableViewHeaderFooterView
+        header.textLabel?.font = UIFont(name: "AvenirNext-DemiBold", size: 14)
+        header.textLabel?.textColor = Color.RED
     }
     
 }
